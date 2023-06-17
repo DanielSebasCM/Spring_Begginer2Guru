0a710a3ac878b4ab666f6b3cb90a0e054c33ebc7
DanielSebasCM <45676844+DanielSebasCM@users.noreply.github.com>
2023-06-16 21:17:51 -0600

    First commit

---
+++ spring5WebAPP/src/test/java/learning/spring/spring5WebAPP/Spring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.spring5WebAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

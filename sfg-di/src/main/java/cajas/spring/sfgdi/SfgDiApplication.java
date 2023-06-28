c036cae73056511febd2181545cff0eafafafb85
Daniel Cajas <dcajas@natgas.com.mx>
2023-06-28 14:46:40 -0600

    Profiles Tests

--- sfg-di/src/main/java/cajas/spring/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/cajas/spring/sfgdi/SfgDiApplication.java
@@ -5,15 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import cajas.spring.sfgdi.controllers.MyController;
+import cajas.spring.sfgdi.controllers.PetController;
 import cajas.spring.sfgdi.controllers.PropertyInjectedController;
 import cajas.spring.sfgdi.controllers.SetterInjectedController;
 import cajas.spring.sfgdi.controllers.ConstructorInjectedController;
+import cajas.spring.sfgdi.controllers.I18nController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 		System.out.println(myController.sayHello());

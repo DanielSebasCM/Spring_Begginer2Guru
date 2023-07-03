43533fa12f6f36311f841a6da1568bec94e155b7
Daniel Cajas <dcajas@natgas.com.mx>
2023-07-03 14:42:58 -0600

    Impl SfgConfig object

--- sfg-di/src/main/java/cajas/spring/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/cajas/spring/sfgdi/SfgDiApplication.java
@@ -9,6 +9,7 @@ import cajas.spring.sfgdi.controllers.PetController;
 import cajas.spring.sfgdi.controllers.PropertyInjectedController;
 import cajas.spring.sfgdi.controllers.SetterInjectedController;
 import cajas.spring.sfgdi.datasource.FakeDataSource;
+import cajas.spring.sfgdi.config.SfgConfig;
 import cajas.spring.sfgdi.controllers.ConstructorInjectedController;
 import cajas.spring.sfgdi.controllers.I18nController;
 
@@ -22,28 +23,40 @@ public class SfgDiApplication {
 		System.out.println("--- The Best Pet is ---");
 		System.out.println(petController.whichPetIsTheBest());
 
+		System.out.println("--- Lnaguage Profile ---");
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
 
+		System.out.println("--- Default injection Bean ---");
 		MyController myController = (MyController) ctx.getBean("myController");
 		System.out.println(myController.sayHello());
 
+		System.out.println("--- Property injection ---");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx
 				.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
+		System.out.println("--- Setter injection ---");
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx
 				.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
 
+		System.out.println("--- Constructor injection ---");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx
 				.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
 
+		System.out.println("--- Fake Data Source ---");
 		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUsername());
 		System.out.println(fakeDataSource.getPassword());
 		System.out.println(fakeDataSource.getJdbcurl());
+
+		System.out.println("--- Sfg Config ---");
+		SfgConfig sfgConfig = ctx.getBean(SfgConfig.class);
+		System.out.println(sfgConfig.getUsername());
+		System.out.println(sfgConfig.getPassword());
+		System.out.println(sfgConfig.getJdbcurl());
 	}
 
 }
